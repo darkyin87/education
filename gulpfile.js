2a701817c5b6462780a22ec37f87ca75afe9635f
Arun Dharumar <adharumar@us-adharumar1.local>
2016-06-30 13:21:37 -0700

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,71 @@
+/* gulpfile.js */
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    nunjucksRender = require('gulp-nunjucks-render');
+
+
+// source and distribution folder
+var source = 'src/',
+    dest = 'dist/';
+
+// Bootstrap scss source
+var bootstrapSass = {
+        in: './node_modules/bootstrap-sass/'
+    };
+
+// Bootstrap fonts source
+var fonts = {
+        in: [source + 'fonts/*.*', bootstrapSass.in + 'assets/fonts/**/*'],
+        out: dest + 'fonts/'
+    };
+
+// Our scss source folder: .scss files
+var scss = {
+    in: source + 'scss/main.scss',
+    out: dest + 'css/',
+    watch: source + 'scss/**/*',
+    sassOpts: {
+        outputStyle: 'nested',
+        precison: 3,
+        errLogToConsole: true,
+        includePaths: [bootstrapSass.in + 'assets/stylesheets']
+    }
+};
+
+// copy bootstrap required fonts to dest
+gulp.task('fonts', function () {
+    return gulp
+        .src(fonts.in)
+        .pipe(gulp.dest(fonts.out));
+});
+
+
+// compile scss
+gulp.task('sass', ['fonts'], function () {
+    return gulp.src(scss.in)
+        .pipe(sass(scss.sassOpts))
+        .pipe(gulp.dest(scss.out));
+});
+
+gulp.task('copy', function() {
+    gulp.src(source + 'index.html')
+    // Perform minification tasks, etc here
+    .pipe(gulp.dest(dest));
+});
+
+// compile html templates
+gulp.task('nunjucks', function () {
+  return gulp.src('src/pages/*.html')
+    .pipe(nunjucksRender({
+      path: ['src/templates'] // String or Array
+    }))
+    .pipe(gulp.dest('dist/'));
+});
+
+// watch sass files
+gulp.task('watch', ['sass'], function () {
+     gulp.watch(scss.watch, ['sass'])
+});
+
+// default task
+gulp.task('default', ['watch']);
